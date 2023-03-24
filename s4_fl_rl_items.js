cb9bcc796c6e6a7a8536e5b9c9f32c907449f0a9
catmorx <alismora3@gmail.com>
2023-03-24 16:28:26 -0500

    Add Error Handling Module

--- s4_fl_rl_items.js
+++ s4_fl_rl_items.js
@@ -3,25 +3,31 @@
  *@NScriptType Restlet
  *@author Catalina R
  */
-define(["N/search"], (search) =>{
-
+const errorModule = "SuiteScripts/-n_Amazon/errorModule.js"
+const integrationLog = "SuiteScripts/-n_Amazon/s4_sns_api_log.js"
+define(['N/search', integrationLog, errorModule], (search, integration, errorModule) => {
     const handlers = {}
     /* si ponemos _get nos da error ya que el espera un literal "get", sin el guion */
     handlers.get = (context) => {
         const response = { code: 400, success: false, data: [], error: [] }
         try {
-            /* posiblemente se le agregue el nivel de precio */
+            const item = context.id;
             const type = "item";
+            
             const columns = [search.createColumn({ name: "itemid", sort: search.Sort.ASC, label: "Nombre" })]
             columns.push(search.createColumn({ name: "salesdescription", label: "Descripción" }))
             columns.push(search.createColumn({ name: "type", label: "Tipo" }))
             columns.push(search.createColumn({ name: "baseprice", label: "Precio base" }))
             columns.push(search.createColumn({ name: "quantityavailable", label: "Disponible" }))
-            const filters = [["type","anyof","InvtPart","NonInvtPart"]]
-            filters.push("AND", ["quantityavailable","greaterthan","0"])
-
+            const filters = [["type", "anyof", "InvtPart", "NonInvtPart"]]
+            filters.push("AND", ["quantityavailable", "greaterthan", "0"])
+            if(item) {
+                filters.push("AND", ["internalid", "anyof", item])
+            }
+            let existSearch = false;
             var itemSearchObj = search.create({ type: type, filters: filters, columns: columns });
             itemSearchObj.run().each(rs => {
+                existSearch = true; 
                 const obj = new Object();
                 obj.internalId = rs.id;
                 obj.itemId = rs.getValue("itemid")
@@ -34,16 +40,19 @@ define(["N/search"], (search) =>{
                 response.success = true;
                 return true;
             });
+            existSearch ? "": errorModule.handleError("No se encontraron articulos en su busqueda")
+            const t1 = "adwdwdw";
+            const t2 = "ad222dw";
+            let x = integration.handleRequest(context, t1, t2);
+
         } catch (error) {
             log.error("error", error.message)
             response.code = 500;
             response.success = false;
             response.error.push(error.message);
-        }finally{
+        } finally {
             return JSON.stringify(response);
         }
     }
-
-
     return handlers
 });
