4f0912fbd8081462c6b487a67a25b58042ce639d
catmorx <alismora3@gmail.com>
2023-03-23 19:06:02 -0500

    Save Invoice ok

--- s4_fl_sales_invoice.js
+++ s4_fl_sales_invoice.js
@@ -3,57 +3,112 @@
  *@NScriptType Restlet
  *@author Catalina R
  */
-define(["N/search"], search => {
+define(["N/search", "N/record"], (search, record) => {
 
     const handlers = {}
+    const searchSalesOrder = response => {
+        const type = "salesorder";
+        const filters = [["type", "anyof", "SalesOrd"]];
+        filters.push("AND", ["fulfillingtransaction.status", "anyof", "ItemShip:B", "ItemShip:A", "ItemShip:C"])
+        filters.push("AND", ["revrectemplate.name", "anyof", "@NONE@"])
+        const columns = [search.createColumn({ name: "entity", label: "Nombre" })]
+        columns.push(search.createColumn({ name: "statusref", join: "fulfillingTransaction" }))
 
+        let salesOrderSearchObj = search.create({ type: type, filters: filters, columns: columns, });
+        salesOrderSearchObj.run().each(res => {
+            const obj = new Object();
+            obj.internalId = res.id;
+            obj.company = res.getText("entity");
+            obj.status = res.getValue({
+                name: "statusref",
+                join: "fulfillingTransaction",
+            })
+            response.data.push(obj);
+            response.code = 200;
+            response.success = true;
+            return true;
+        });
+    }
+    const searchInvoice = objCreated => {
+        const type = "invoice"
+        const filters = [["type", "anyof", "CustInvc"]]
+        filters.push("AND", ["createdfrom", "anyof", objCreated])
+        filters.push("AND", ["mainline", "is", "T"])
+        const columns = [search.createColumn({ name: "createdfrom", label: "Creado desde" })]
+        columns.push(search.createColumn({ name: "statusref", label: "Estado" }))
+        var invoiceSearchObj = search.create({ type: type, filters: filters, columns: columns });
+        /*  
+        !puede haber mas de 1 factura por orden de venta, validar
+        */
+        const alreadyExists = []
+        invoiceSearchObj.run().each(result => {
+            let ready = result.getValue("createdfrom")
+            alreadyExists.push(ready);
+            return true;
+        });
+        return alreadyExists
+    }
     handlers.get = (context) => {
         const response = { code: 400, success: false, data: [], error: [] }
         try {
-            const type = "itemfulfillment";
-            const filters = [["type", "anyof", "ItemShip"]];
-            filters.push("AND", ["createdfrom", "anyof", "176"])
-            filters.push("AND", ["mainline", "is", "T"])
-            const columns = [search.createColumn({ name: "statusref", label: "Estado" })]
-            columns.push(search.createColumn({ name: "entity", label: "Nombre" }))
-            columns.push(search.createColumn({ name: "trandate", label: "Fecha" }))
-            columns.push(search.createColumn({name: "createdfrom", label: "Creado desde"}))
-            let itemfulfillmentSearchObj = search.create({ type: type, filters: filters, columns: columns, });
-            itemfulfillmentSearchObj.run().each(res => {
-                log.debug("Entra")
-                const obj = new Object();
-                obj.internalId = res.id;
-                obj.name = res.getValue("entity");
-                obj.createdFrom = res.getValue("createdfrom");
-                obj.trandate = res.getValue("trandate")
-                obj.status = res.getValue("statusref")
-                response.data.push(obj);
-                response.code = 200;
-                response.success = true;
-                return true;
-            });
-            // log.debug("search", itemfulfillmentSearchObj)
+            searchSalesOrder(response)
         } catch (error) {
             log.error("error", error.message);
             response.code = 500;
             response.success = false;
             response.error.push(error.message);
-        }finally{
+        } finally {
             return JSON.stringify(response);
         }
     }
 
-    // handlers.post = (context) => {
-
-    // }
-
-    // // handlers.put=(context) =>{
-
-    // // }
-
-    // // handlers.delete=(context) =>{
-
-    // // }
+    handlers.post = (context) => {
+        const response = { code: 400, success: false, data: [], error: [] }
+        try {
+            let res = JSON.parse(handlers.get(context));
+            let data = res.data;
+            let ss = []
+            data.forEach(obj => {
+                let alreadyExists = searchInvoice(obj.internalId)
+                if (alreadyExists.length == 0) {
+                    ss.push(obj.internalId);
+                }
+            });
+            /* ... para que podamos acceder a los numeros, es como si quitaramos el [] */
+            let unique = ss.reduce((prev, curr) => prev.indexOf(curr) === -1 ? [...prev, curr] : prev, []);
+            unique.forEach(u => {
+                u = Number(u)
+                let load = record.load({
+                    type: "salesorder",
+                    id: u,
+                    isDynamic: true,
+                });
+                let value = load.getValue("paymentmethod");
+                if (value.length > 0) {
+                    /* si tiene metodo de pago se crea la cashsale */
+                    response.data.push("crea cashsale", u);
+                } else {
+                    let see = record.transform({
+                        fromType: "salesorder",
+                        fromId: u,
+                        toType: "invoice",
+                        isDynamic: true,
+                    })
+                    const x = see.save()
+                    response.data.push("Factura de venta creada");
+                }
+            })
+            response.code = 200;
+            response.success = true;
+        } catch (error) {
+            log.error("error", error);
+            response.code = 500;
+            response.success = false;
+            response.error.push(error.message);
+        } finally {
+            return JSON.stringify(response);
+        }
+    }
 
     return handlers;
 });
