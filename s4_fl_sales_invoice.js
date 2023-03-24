cde01c1be8ef08d805c59ae170b716b0d9c10654
catmorx <alismora3@gmail.com>
2023-03-24 15:29:53 -0500

    Add Error Handling Module

--- s4_fl_sales_invoice.js
+++ s4_fl_sales_invoice.js
@@ -3,32 +3,38 @@
  *@NScriptType Restlet
  *@author Catalina R
  */
-define(["N/search", "N/record"], (search, record) => {
+const errorModule = "SuiteScripts/-n_Amazon/errorModule.js"
+define(["N/search", "N/record", errorModule], (search, record, errorModule) => {
 
     const handlers = {}
+
     const searchSalesOrder = response => {
         const type = "salesorder";
         const filters = [["type", "anyof", "SalesOrd"]];
         filters.push("AND", ["fulfillingtransaction.status", "anyof", "ItemShip:B", "ItemShip:A", "ItemShip:C"])
         filters.push("AND", ["revrectemplate.name", "anyof", "@NONE@"])
+        // filters.push("AND", ["mainline", "is", "T"])
         const columns = [search.createColumn({ name: "entity", label: "Nombre" })]
         columns.push(search.createColumn({ name: "statusref", join: "fulfillingTransaction" }))
-
         let salesOrderSearchObj = search.create({ type: type, filters: filters, columns: columns, });
+        let existSearch = false;
         salesOrderSearchObj.run().each(res => {
             const obj = new Object();
+            existSearch = true;
             obj.internalId = res.id;
             obj.company = res.getText("entity");
             obj.status = res.getValue({
                 name: "statusref",
                 join: "fulfillingTransaction",
             })
-            response.data.push(obj);
+            response.data.push(obj)
             response.code = 200;
             response.success = true;
             return true;
         });
+        existSearch ? "" : errorModule.handleError("Sin resultados en la busqueda de Ordenes de Venta")
     }
+
     const searchInvoice = objCreated => {
         const type = "invoice"
         const filters = [["type", "anyof", "CustInvc"]]
@@ -41,13 +47,16 @@ define(["N/search", "N/record"], (search, record) => {
         !puede haber mas de 1 factura por orden de venta, validar
         */
         const alreadyExists = []
+        let existSearch = false;
         invoiceSearchObj.run().each(result => {
+            let existSearch = true;
             let ready = result.getValue("createdfrom")
             alreadyExists.push(ready);
             return true;
         });
         return alreadyExists
     }
+
     handlers.get = (context) => {
         const response = { code: 400, success: false, data: [], error: [] }
         try {
@@ -66,16 +75,20 @@ define(["N/search", "N/record"], (search, record) => {
         const response = { code: 400, success: false, data: [], error: [] }
         try {
             let res = JSON.parse(handlers.get(context));
+            // response.data.push(res)
+            res.code == 400 ? errorModule.handleError("No se encontró ordenes pendientes de facturación") : ""
+            res.code == 500 ? errorModule.handleError(res.error) : ""
+
             let data = res.data;
-            let ss = []
+            let pendingOrders = []
             data.forEach(obj => {
                 let alreadyExists = searchInvoice(obj.internalId)
                 if (alreadyExists.length == 0) {
-                    ss.push(obj.internalId);
+                    pendingOrders.push(obj.internalId);
                 }
             });
             /* ... para que podamos acceder a los numeros, es como si quitaramos el [] */
-            let unique = ss.reduce((prev, curr) => prev.indexOf(curr) === -1 ? [...prev, curr] : prev, []);
+            let unique = pendingOrders.reduce((prev, curr) => prev.indexOf(curr) === -1 ? [...prev, curr] : prev, []);
             unique.forEach(u => {
                 u = Number(u)
                 let load = record.load({
@@ -95,10 +108,10 @@ define(["N/search", "N/record"], (search, record) => {
                         isDynamic: true,
                     })
                     const x = see.save()
-                    response.data.push("Factura de venta creada");
+                    response.data.push("Factura de venta ha sido creada exitosamente", x);
                 }
             })
-            response.code = 200;
+            response.code = 201;
             response.success = true;
         } catch (error) {
             log.error("error", error);
