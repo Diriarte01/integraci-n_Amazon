a6230bf906ca7584179a9736928935fef1db4495
abrieva <abrieva@sol4it.net>
2023-03-27 08:44:27 -0500

    Validando campos y Modularizando las busquedas

--- s4_fl_rl_salesorder.js
+++ s4_fl_rl_salesorder.js
@@ -2,128 +2,120 @@
  *@NApiVersion 2.1
  *@NScriptType Restlet
  */
-define(['N/search','N/record'], function (search,record) {
-
-    const handlers = {}
-
-
-    handlers.get = (context) => {
-
-        const response = { code: 400, success: false, data: [], error: [], message: '' }
-        try {
-            
-            const type = "salesorder";
-            const columns = [search.createColumn({ name: "internalid", label: "ID interno" })]
-            columns.push(search.createColumn({ name: "type", label: "Tipo" }))
-            columns.push(search.createColumn({ name: "tranid", label: "Número de documento" }))
-            columns.push(search.createColumn({ name: "entity", label: "Nombre" }))
-            columns.push(search.createColumn({ name: "salesrep", label: "Representante de ventas" }))
-            columns.push(search.createColumn({name: "trandate", label: "Fecha"}))
-            columns.push(search.createColumn({name: "asofdate", label: "Fecha de corte"}))
-            columns.push(search.createColumn({name: "statusref", label: "Estado"}))
-            columns.push(search.createColumn({ name: "account", label: "Cuenta" }))
-            columns.push(search.createColumn({ name: "total", label: "Total" }))
-            columns.push(search.createColumn({ name: "subsidiary", label: "Subsidiaria" }))
-            columns.push(search.createColumn({ name: "shipaddress", label: "Dirección de envío" }))
-            const filters = [["type", "anyof", "SalesOrd"]]
-            filters.push("AND", ["mainline", "is", "T"])
-            let salesOrderSearchObj = search.create({
-                type: type,
-                filters: filters,
-                columns: columns
-            })
-            salesOrderSearchObj.run().each(rs => {
-                const obj = new Object();
-                obj.internalId = rs.id;
-                obj.type = rs.getValue("type");
-                obj.tranid = rs.getValue("tranid");
-                obj.entity = rs.getValue("entity");
-                obj.salesrep = rs.getValue("salesrep");
-                obj.trandate = rs.getValue("trandate");
-                obj.asofdate = rs.getValue("asofdate");
-                obj.statusref = rs.getValue("statusref");
-                obj.account = rs.getValue("account");
-                obj.total = rs.getValue("total");
-                obj.subsidiary = rs.getValue("subsidiary");
-                obj.shipaddress = rs.getValue("shipaddress");
-                response.data.push(obj);
-                response.code = 200;
-                response.success = true;
-                return true;
-            })
-        } catch (error) {
-            log.error("error", error.message)
-            response.code = 500;
-            response.success = false;
-            response.error.push(error.message);
-        } finally {
-            return JSON.stringify(response);
-        }
-    }
-
-    handlers.post = (context) => {
-
-        const response = { code: 400, success: false, data: [], error: [], message: '' }
-
-        try {
-            const data = context.data;
-            log.debug('Creando Orden de Venta', data)
-            const salesOrder = record.create({
-                type: record.Type.SALES_ORDER,
-            })
-            salesOrder.setValue({
-                fieldId: 'entity',
-                value: data.name
-            })
-            for(let i = 0; i < data.items.length; i++){
-                salesOrder.setSublistValue({   
-                    sublistId: 'item',
-                    fieldId: 'item',
-                    line: i,
-                    value: data.items[i].item
-                })
-                salesOrder.setSublistValue({
-                    sublistId: 'item',
-                    fieldId: 'quantity',
-                    line: i,
-                    value: data.items[i].quantity
-                })
-            }
-            
-            salesOrder.setValue({
-                fieldId:'location',
-                value: data.location
-            })
-
-            const salesOrderId = salesOrder.save()
-            response.data.push({
-                internalId: salesOrderId,
-                typerecord: record.Type.SALES_ORDER
-            })
-            response.code = 200
-            response.success = true;
-            response.message = 'Se creo la orden de venta correctamente'
-
-
-        } catch (error) {
-            log.error("error", error.message)
-            response.code = 500;
-            response.success = false;
-            response.error.push(error.message);
-        } finally {
-            return JSON.stringify(response)
-        }
-
-
-    }
-
-    handlers.put = (context) => {
-
-    }
-
-    handlers.delete = (context) => {
-
-    }
-
-    return handlers
-})
+ const errorModule = "SuiteScripts/-n_Amazon/errorModule.js"
+ const searchs = "SuiteScripts/-n_Amazon/SearchModule.js"
+ define(['N/search', 'N/record', 'N/https', 'N/runtime', errorModule,searchs], function (search, record, https, runtime, errorM,searchM) {
+ 
+     const handlers = {}
+ 
+ 
+     handlers.get = (context) => {
+         
+         let response = {}
+         try {
+             
+             response = searchM.busquedas(context,"salesorder")
+ 
+         } catch (error) {
+             log.error("error", error.message)
+             response.code = 500;
+             response.success = false;
+             response.error.push(error.message);
+         } finally {
+             return JSON.stringify(response)
+         }
+     }
+ 
+     handlers.post = (context) => {
+ 
+         const response = { code: 400, success: false, data: [], error: []}
+         let boolean = true
+         try {
+             const data = context.data;
+             log.debug('Creando Orden de Venta', context)
+             const salesOrder = record.create({
+                 type: record.Type.SALES_ORDER,
+             })
+             Number.isInteger(data.name) ? "" : errorM.handleError("!!Ha ingresado un Tipo de dato erroneo para el campo de cliente, se debe ingresar el ID del cliente")
+             //const s4_fl_rl_customer = `https://tstdrv2719149.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=154&deploy=2&id=${context.data.name}`
+             let responseCustomer = {}
+             let params = {
+                 "id": data.name
+             }
+             responseCustomer = searchM.busquedas(params,"customer")
+             if(responseCustomer.code === 500){
+                 response.code = responseCustomer.code
+                 response.success = responseCustomer.success
+                 response.error.push(responseCustomer.error)
+                 boolean = false
+             } else {
+                 salesOrder.setValue({
+                     fieldId: 'entity',
+                     value: data.name
+                 })
+                 boolean = false
+             }
+             
+             for (let i = 0; i < data.items.length; i++) {
+                 Number.isInteger(data.items[i].item) ? "" : errorM.handleError("!!Ha ingresado un Tipo de dato erroneo para el campo de item, se debe ingresar el ID del item")
+                 let responseItem = {}
+                 params = {
+                     "id": data.items[i].item
+                 }
+                 responseItem = searchM.busquedas(params,"item")
+                 if(responseItem.code === 500){
+                     response.code = responseItem.code
+                     response.success = responseItem.success
+                     response.error.push(responseItem.error)
+                     boolean = false
+                 } else {
+                     salesOrder.setSublistValue({
+                         sublistId: 'item',
+                         fieldId: 'item',
+                         line: i,
+                         value: data.items[i].item
+                     })
+                 }
+                 Number.isInteger(data.items[i].quantity) ? "" : errorM.handleError("!!Ha ingresado un Tipo de dato erroneo para el campo de cantidad, se debe ingresar un numero")
+                 salesOrder.setSublistValue({
+                     sublistId: 'item',
+                     fieldId: 'quantity',
+                     line: i,
+                     value: data.items[i].quantity
+                 })
+             }
+             Number.isInteger(data.location) ? "" : errorM.handleError("!!!Ha ingresado un Tipo de dato erroneo para el campo de ubicacion, se debe ingresar el ID de la ubicacion")
+             salesOrder.setValue({
+                 fieldId: 'location',
+                 value: data.location
+             })
+             if(boolean === true){
+                 const salesOrderId = salesOrder.save()
+                 response.code = 200
+                 response.success = true;
+                 response.message = 'Se creo la orden de venta correctamente'
+             }
+             
+             
+             //errorM.handleError("No se pudo crear la orden de venta") 
+         } catch (error) {
+             log.error("error", error.message)
+             response.code = 500;
+             response.success = false;
+             response.error.push(error.message);
+         } finally {
+             return JSON.stringify(response)
+         }
+ 
+     }
+ 
+     handlers.put = (context) => {
+ 
+     }
+ 
+     handlers.delete = (context) => {
+ 
+     }
+ 
+     return handlers
+ })
